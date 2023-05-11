259ecbdbec7494a4e2852cc167ff0f078d45d097
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-11 13:14:37 -0600

    Update 0001_Problemas_De_Satisfaccion_De_Restricciones.py

--- 2.1.2_Enfoque_Grafos/Satisfaccion_de_Restricciones/0001_Problemas_De_Satisfaccion_De_Restricciones.py
+++ 2.1.2_Enfoque_Grafos/Satisfaccion_de_Restricciones/0001_Problemas_De_Satisfaccion_De_Restricciones.py
@@ -1,84 +1,49 @@
-#Alan de Jesus Fabian Garcia
-from typing import Dict, List, Optional
-
-class Variable:
-    def __init__(self, name: str, domain: List[int]) -> None:
-        self.name = name
-        self.domain = domain
-        self.value: Optional[int] = None
-
-class Constraint:
-    def __init__(self, variables: List[Variable], constraint_function) -> None:
-        self.variables = variables
-        self.constraint_function = constraint_function
-
-    def satisfied(self) -> bool:
-        return self.constraint_function([variable.value for variable in self.variables])
+#Alan de Jesus Fabian Garcia 
+from typing import List, Dict
 
 class CSP:
-    def __init__(self, variables: List[Variable], constraints: List[Constraint]) -> None:
+    def __init__(self, variables: List[str], domains: Dict[str, List[int]], constraints: List[str]):
         self.variables = variables
+        self.domains = domains
         self.constraints = constraints
 
-    def backtrack(self, assignment: Dict[Variable, int], graph: Dict[Variable, List[Variable]]) -> Optional[Dict[Variable, int]]:
+    def backtracking_search(self, assignment={}):
+        # Si se han asignado todas las variables, se ha alcanzado una solución
         if len(assignment) == len(self.variables):
             return assignment
-        
-        unassigned = [variable for variable in self.variables if variable not in assignment]
-        first = unassigned[0]
-        ordered_domain = self.order_domain_values(first, assignment, graph)
-        
-        for value in ordered_domain:
-            first.value = value
-            if self.forward_checking(first, graph):
-                assignment[first] = value
-                result = self.backtrack(assignment, graph)
+
+        # Selecciona la variable sin asignar con el dominio más reducido
+        unassigned_variables = [v for v in self.variables if v not in assignment]
+        selected_variable = min(unassigned_variables, key=lambda v: len(self.domains[v]))
+
+        # Intenta asignar un valor a la variable seleccionada
+        for value in self.domains[selected_variable]:
+            consistent = True
+
+            # Comprueba si la asignación es consistente con las restricciones
+            for constraint in self.constraints:
+                if selected_variable in constraint and constraint not in assignment:
+                    continue
+                if not constraint_satisfied(constraint, assignment):
+                    consistent = False
+                    break
+
+            # Si la asignación es consistente, continúa con la siguiente variable
+            if consistent:
+                assignment[selected_variable] = value
+                result = self.backtracking_search(assignment)
                 if result is not None:
                     return result
-                assignment.pop(first)
-            first.value = None
-            
+
+            # Si la asignación no es consistente, deshace la asignación y prueba otro valor
+            del assignment[selected_variable]
+
+        # Si no se puede encontrar una solución, devuelve None
         return None
 
-    def forward_checking(self, variable: Variable, graph: Dict[Variable, List[Variable]]) -> bool:
-        for neighbor in graph[variable]:
-            constraint = [c for c in self.constraints if variable in c.variables and neighbor in c.variables]
-            if neighbor.value is None:
-                for value in neighbor.domain:
-                    variable.value = value
-                    if not any(constraint) or all(c.satisfied() for c in constraint):
-                        break
-                else:
-                    return False
-            variable.value = None
+def constraint_satisfied(constraint, assignment):
+    # Comprueba si la asignación satisface una restricción dada
+    x, y = constraint.split("<")
+    if x not in assignment or y not in assignment:
         return True
-
-    def order_domain_values(self, variable: Variable, assignment: Dict[Variable, int], graph: Dict[Variable, List[Variable]]) -> List[int]:
-        values = []
-        for value in variable.domain:
-            variable.value = value
-            if self.forward_checking(variable, graph):
-                values.append(value)
-        variable.value = None
-        return values
-
-def main() -> None:
-    variables = [Variable('A', [1, 2, 3]), Variable('B', [1, 2, 3]), Variable('C', [1, 2, 3])]
-    constraints = [
-        Constraint([variables[0], variables[1]], lambda values: values[0] != values[1]),
-        Constraint([variables[1], variables[2]], lambda values: values[1] != values[2])
-    ]
-    graph = {
-        variables[0]: [variables[1]],
-        variables[1]: [variables[0], variables[2]],
-        variables[2]: [variables[1]]
-    }
-    csp = CSP(variables, constraints)
-    result = csp.backtrack({}, graph)
-    if result is None:
-        print('No se encontró una solución')
-    else:
-        print(result)
-
-if __name__ == '__main__':
-    main()
+    return assignment[x] < assignment[y]
