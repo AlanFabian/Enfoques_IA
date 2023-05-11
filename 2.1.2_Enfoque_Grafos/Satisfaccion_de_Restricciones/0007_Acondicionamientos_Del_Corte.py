e8b85d3071e5d53262d394d56e9b4b05f04d8c94
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-11 13:35:44 -0600

    Update 0007_Acondicionamientos_Del_Corte.py

--- 2.1.2_Enfoque_Grafos/Satisfaccion_de_Restricciones/0007_Acondicionamientos_Del_Corte.py
+++ 2.1.2_Enfoque_Grafos/Satisfaccion_de_Restricciones/0007_Acondicionamientos_Del_Corte.py
@@ -1,1 +1,84 @@
+#Alan de Jesus Fabian Garcia 
+import random
 
+def calcular_conflictos(estado, grafo):
+    conflictos = 0
+    for nodo in estado:
+        for vecino in grafo[nodo]:
+            if estado[nodo] == estado[vecino]:
+                conflictos += 1
+    return conflictos
+
+def acondicionar_corte(nodo, valor, estado_actual, grafo):
+    for vecino in grafo[nodo]:
+        if vecino in estado_actual:
+            continue
+        for posible_valor in grafo[vecino]:
+            if posible_valor == valor:
+                return False
+    return True
+
+def minimos_conflictos(estado_inicial, grafo, max_iteraciones):
+    estado_actual = estado_inicial.copy()
+
+    for _ in range(max_iteraciones):
+        if calcular_conflictos(estado_actual, grafo) == 0:
+            return estado_actual
+
+        nodos_con_conflicto = []
+        for nodo in estado_actual:
+            for vecino in grafo[nodo]:
+                if estado_actual[nodo] == estado_actual[vecino]:
+                    nodos_con_conflicto.append(nodo)
+                    break
+
+        if len(nodos_con_conflicto) > 0:
+            nodo_conflicto = random.choice(nodos_con_conflicto)
+            vecinos = grafo[nodo_conflicto]
+
+            mejores_estados = []
+            min_conflictos = float('inf')
+
+            for valor in vecinos:
+                if acondicionar_corte(nodo_conflicto, valor, estado_actual, grafo):
+                    estado_actual[nodo_conflicto] = valor
+                    conflictos = calcular_conflictos(estado_actual, grafo)
+
+                    if conflictos < min_conflictos:
+                        mejores_estados = [valor]
+                        min_conflictos = conflictos
+                    elif conflictos == min_conflictos:
+                        mejores_estados.append(valor)
+
+            estado_actual[nodo_conflicto] = random.choice(mejores_estados)
+        else:
+            nodo = random.choice(list(grafo.keys()))
+            valor = random.choice(grafo[nodo])
+            estado_actual[nodo] = valor
+
+    return None
+
+# Ejemplo de uso
+grafo = {
+    'A': ['B', 'C'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D'],
+    'D': ['B', 'C']
+}
+
+estado_inicial = {
+    'A': 'Rojo',
+    'B': 'Azul',
+    'C': 'Verde',
+    'D': 'Rojo'
+}
+
+max_iteraciones = 100
+
+estado_final = minimos_conflictos(estado_inicial, grafo, max_iteraciones)
+
+if estado_final is None:
+    print("No se encontró una solución en el número máximo de iteraciones.")
+else:
+    print("Estado final:")
+    print(estado_final)
