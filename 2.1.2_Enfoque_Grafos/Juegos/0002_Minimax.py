98c13565e69c5f184c0d20c0a14b036b73f0a036
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-11 13:04:22 -0600

    Update 0002_Minimax.py

--- 2.1.2_Enfoque_Grafos/Juegos/0002_Minimax.py
+++ 2.1.2_Enfoque_Grafos/Juegos/0002_Minimax.py
@@ -1,1 +1,176 @@
+#Alan de Jesus Fabian Garcia 
+import random
+
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]
+        self.current_winner = None
+
+    def print_board(self):
+        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
+        for row in number_board:
+            print("| " + " | ".join(row) + " |")
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == " "]
+
+    def empty_squares(self):
+        return " " in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(" ")
+
+    def make_move(self, square, letter):
+        if self.board[square] == " ":
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        # check row
+        row_ind = square // 3
+        row = self.board[row_ind * 3:(row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        # check column
+        col_ind = square % 3
+        column = [self.board[col_ind + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        # check diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+        # no winner
+        return False
+
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = "X"
+    while game.empty_squares():
+        if letter == "O":
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f" makes a move to square {square}")
+                game.print_board()
+                print("")
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + " wins!")
+                return letter
+
+            letter = "O" if letter == "X" else "X"
+
+    if print_game:
+        print("It's a tie!")
+
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "'s turn. Input move (0-8): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again.")
+        return val
+
+
+def minimax(board, depth, maximizing_player, computer_letter, human_letter):
+    if board.current_winner == computer_letter:
+        return (None, 100000000000000)
+    elif board.current_winner == human_letter:
+        return (None, -100000000000000)
+    elif board.game_over():
+        return (None, 0)
+
+    if maximizing_player:
+        max_eval = float('-inf')
+        best_move = None
+        for move in board.available_moves():
+            board.make_move(move, computer_letter)
+            eval = minimax(board, depth+1, False, computer_letter, human_letter)[1]
+            board.undo_move(move)
+            if eval > max_eval:
+                max_eval = eval
+                best_move = move
+        return (best_move, max_eval)
+    else:
+        min_eval = float('inf')
+        best_move = None
+        for move in board.available_moves():
+            board.make_move(move, human_letter)
+            eval = minimax(board, depth+1, True, computer_letter, human_letter)[1]
+            board.undo_move(move)
+            if eval < min_eval:
+                min_eval = eval
+                best_move = move
+        return (best_move, min_eval)
+
+
+class ComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = minimax(game, 0, True, self.letter, game.current_player)[0]
+        return square
+
+
+
+
+
+
 
