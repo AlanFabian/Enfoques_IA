f860d9fa213d534a226d1ced8c18726cfa1c803a
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-12 18:12:38 -0600

    Update 0004_Separabilidad_Lineal.py

--- 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0004_Separabilidad_Lineal.py
+++ 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0004_Separabilidad_Lineal.py
@@ -1,1 +1,29 @@
+#Alan de Jesus Fabian Garcia 
+import numpy as np
 
+class LinearClassifier:
+    def __init__(self, learning_rate=0.01, epochs=100):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        # Inicializar los pesos y el sesgo con valores aleatorios pequeños
+        self.weights = np.random.randn(X.shape[1])
+        self.bias = np.random.randn()
+
+        for _ in range(self.epochs):
+            # Calcular la salida del modelo
+            y_pred = self.predict(X)
+
+            # Actualizar los pesos y el sesgo utilizando el algoritmo del perceptrón
+            update = self.learning_rate * np.dot((y - y_pred), X)
+            self.weights += update
+            self.bias += self.learning_rate * np.sum(y - y_pred)
+
+    def predict(self, X):
+        # Calcular la salida del modelo
+        z = np.dot(X, self.weights) + self.bias
+        y_pred = np.where(z >= 0, 1, -1)
+        return y_pred
