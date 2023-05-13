361c552408e306a4dd512b4fd18d1f29ce8742f1
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-12 18:15:23 -0600

    Update 0005_Redes_Multicapa.py

--- 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0005_Redes_Multicapa.py
+++ 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0005_Redes_Multicapa.py
@@ -1,1 +1,27 @@
+#Alan de Jesus fabian Garcia 
+import tensorflow as tf
+import numpy as np
 
+class MultilayerPerceptron:
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(self.hidden_dim, activation='relu', input_shape=(self.input_dim,)),
+            tf.keras.layers.Dense(self.output_dim, activation='softmax')
+        ])
+
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
+
+    def fit(self, X, y, epochs=100, batch_size=32):
+        y_one_hot = tf.keras.utils.to_categorical(y, num_classes=self.output_dim)
+        self.model.fit(X, y_one_hot, epochs=epochs, batch_size=batch_size)
+
+    def predict(self, X):
+        predictions = self.model.predict(X)
+        return np.argmax(predictions, axis=1)
