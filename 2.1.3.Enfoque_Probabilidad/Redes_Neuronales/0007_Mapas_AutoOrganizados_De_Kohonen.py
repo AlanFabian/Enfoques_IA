053873a3dfa23e7ca58b667d2e5894376b3e6fb4
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-12 18:18:06 -0600

    Update 0007_Mapas_AutoOrganizados_De_Kohonen.py

--- 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0007_Mapas_AutoOrganizados_De_Kohonen.py
+++ 2.1.3.Enfoque_Probabilidad/Redes_Neuronales/0007_Mapas_AutoOrganizados_De_Kohonen.py
@@ -1,1 +1,21 @@
+#Alan de Jesus Fabian Garcia 
+from minisom import MiniSom
+import numpy as np
 
+class KohonenMap:
+    def __init__(self, input_dim, output_shape):
+        self.input_dim = input_dim
+        self.output_shape = output_shape
+        self.map = self.build_map()
+
+    def build_map(self):
+        map = MiniSom(self.output_shape[0], self.output_shape[1], self.input_dim, sigma=1.0, learning_rate=0.5)
+        return map
+
+    def fit(self, X, epochs=100):
+        self.map.random_weights_init(X)
+        self.map.train_random(X, epochs)
+
+    def predict(self, X):
+        predictions = self.map.predict(X)
+        return predictions
