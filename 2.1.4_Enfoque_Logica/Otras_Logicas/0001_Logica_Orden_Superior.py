da84cbf3ef1cf8252bfe59752e7fbb10e665701a
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-14 12:15:41 -0600

    Update 0001_Logica_Orden_Superior.py

--- 2.1.4_Enfoque_Logica/Otras_Logicas/0001_Logica_Orden_Superior.py
+++ 2.1.4_Enfoque_Logica/Otras_Logicas/0001_Logica_Orden_Superior.py
@@ -1,1 +1,17 @@
+#Alan de Jesus Fabian Garcia 
+def apply_func(func, value):
+    return func(value)
 
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+# Ejemplo de uso
+
+result = apply_func(square, 5)
+print("Cuadrado de 5:", result)
+
+result = apply_func(cube, 3)
+print("Cubo de 3:", result)
