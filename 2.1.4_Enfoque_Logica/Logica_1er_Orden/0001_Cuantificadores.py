35ac01c45d18f9716b6cfd6cbdde4333de026352
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-05-14 12:04:23 -0600

    Update 0001_Cuantificadores.py

--- 2.1.4_Enfoque_Logica/Logica_1er_Orden/0001_Cuantificadores.py
+++ 2.1.4_Enfoque_Logica/Logica_1er_Orden/0001_Cuantificadores.py
@@ -1,1 +1,12 @@
+#Alan de Jesus Fabian Garcia 
+from sympy import symbols, ForAll, Not, satisfiable
 
+# Variables simbólicas
+x, y = symbols('x y')
+
+# Fórmula lógica
+formula = ForAll(x, Not(ForAll(y, x != y)))
+
+# Verificar satisfacibilidad
+satisfiable_formula = satisfiable(formula)
+print(satisfiable_formula)  # Devuelve un modelo de asignación de variables que satisface la fórmula
